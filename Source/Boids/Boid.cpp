14ee39d6348e5d810b6ed3541fe517bf24a1625a
LeoSery <74504632+LeoSery@users.noreply.github.com>
2025-02-26 10:22:49 +0100

    Add: Boid base class.

---
+++ Source/Boids/Boid.cpp
@@ -0,0 +1,79 @@
+﻿#include "Boid.h"
+
+ABoid::ABoid()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+	
+	Mesh->SetSimulatePhysics(false);
+	Mesh->SetEnableGravity(false);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	
+	Velocity = 1.0f;
+	Direction = FVector(1.0f, 0.0f, 0.0f);
+}
+
+void ABoid::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABoid::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector NewLocation = CurrentLocation + Direction * Velocity * DeltaTime;
+	SetActorLocation(NewLocation);
+
+	FRotator NewRotation = Direction.Rotation();
+    
+	SetActorRotation(NewRotation);
+	
+	FVector ForwardVector = GetActorForwardVector();
+	FVector UpVector = GetActorUpVector();
+    
+	// Longueur des lignes de debug
+	const float LineLength = 100.0f;
+    
+	// Dessiner Direction (rouge)
+	DrawDebugLine(
+		GetWorld(),
+		CurrentLocation,
+		CurrentLocation + Direction.GetSafeNormal() * LineLength,
+		FColor::Red,
+		false, // Persistant
+		-1.0f, // Durée (négatif = une frame)
+		0,     // Priorité
+		2.0f   // Épaisseur
+	);
+    
+	// Dessiner Forward Vector (bleu)
+	DrawDebugLine(
+		GetWorld(),
+		CurrentLocation,
+		CurrentLocation + ForwardVector * LineLength,
+		FColor::Blue,
+		false,
+		-1.0f,
+		0,
+		2.0f
+	);
+    
+	// Dessiner Up Vector (vert)
+	DrawDebugLine(
+		GetWorld(),
+		CurrentLocation,
+		CurrentLocation + UpVector * LineLength,
+		FColor::Green,
+		false,
+		-1.0f,
+		0,
+		2.0f
+	);
+}
---
+++ Source/Boids/Boid.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Boid.generated.h"
+
+UCLASS()
+class BOIDS_API ABoid : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABoid();
+	
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	float Velocity;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FVector Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY()
+	USceneComponent* Root;
+	
+protected:
+	virtual void BeginPlay() override;
+};
